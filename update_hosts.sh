800d55b5cd96cc6d09572962105ce3c2181f6f7b
CatCloud2019 <47936755+CatCloud2019@users.noreply.github.com>
2019-10-27 22:41:39 +0800

    Update update_hosts.sh

--- update_hosts.sh
+++ update_hosts.sh
@@ -1,17 +1,12 @@
-ipv6=$1
+
 work_path="/root"
 rm $work_path/hosts*
 cp /etc/hosts $work_path/._backhosts
-if [ "$ipv6" = "6" ]
-then
-	 wget https://raw.githubusercontent.com/CatCloud2019/newdos/master/hosts/ipv4/hosts  -O $work_path/hosts
-else
-	wget https://raw.githubusercontent.com/CatCloud2019/newdos/master/hosts/ipv6/hosts -O $work_path/hosts
-fi
+wget https://raw.githubusercontent.com/CatCloud2019/newdos/master/hosts/ipv4/hosts  -O $work_path/hosts
 mv $work_path/hosts /etc/hosts
 chown root:root /etc/hosts
 chmod a+w /etc/hosts
-# 追加你的～下tmp.hosts的文件到系统hosts文件里
+# 追加你的～下hosts的文件到系统hosts文件里
 cat /root/hosts >> /etc/hosts
 
 #使用方法：
