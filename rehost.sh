865618dca0937848f06d593300bd53313df35fbe
CatCloud2019 <47936755+CatCloud2019@users.noreply.github.com>
2019-10-28 18:18:30 +0800

    Update rehost.sh

--- rehost.sh
+++ rehost.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# 2019/10/28
 wget https://raw.githubusercontent.com/CatCloud2019/newdos/master/hosts/ipv4/hosts -O /tmp/hosts&&echo "下载成功"||exit 0
 mv /etc/hosts /etc/hosts.bak
 cp /tmp/hosts /etc/hosts&&echo "已经成功执行"||echo "失败"
